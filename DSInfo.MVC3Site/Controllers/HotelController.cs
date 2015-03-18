f335dd141fd5e05570fb6dd8f38558161d5b38ce
xjshuang <xjshuang@Ctrip.com>
2015-03-18 15:59:58 +0800

    add mvc3 testPage

---
+++ DSInfo.MVC3Site/Controllers/HotelController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DSInfo.MVC3Site.Controllers
+{
+    public class HotelController : Controller
+    {
+        //
+        // GET: /Hotel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult test()
+        {
+            return View();
+        }
+
+    }
+}
